7ffbccd166b34c38ae6c4e67bb6acb0cfad83b59
mostafa59 <37313589+mostafa59@users.noreply.github.com>
2026-01-05 08:34:20 +0200

    Fix import statements in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,1 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import jwt from 'jsonwebtoken';
-import bcryptjs from 'bcryptjs';
-
-dotenv.config();
-
-const app = express();
-const PORT
+import express from '
